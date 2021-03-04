fda5373c4bf22fdbd35cb6ef3928d5d9c3fe15a7
rodolfo_oliveira <rod.vgo@gmail.com>
2021-03-04 00:45:12 -0300

    project initialize

---
+++ src/pages/Home/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="toolbar">
+        <div className="">
+          <a href="/">Conecta Dev - Alteração</a>
+        </div>
+        <div className="">
+          <button>Novo Post</button>
+          <span>img1</span>
+          <span>img2</span>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
